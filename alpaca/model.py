cbe545625e1b8c3bfcb7e35bbe02047e6ecc6437
danielz02 <31590926+danielz02@users.noreply.github.com>
2023-05-18 16:30:32 -0500

    Refactor to test generation

--- alpaca/model.py
+++ alpaca/model.py
@@ -1,18 +1,27 @@
+import json
+import os.path
 from typing import Optional, Union, Tuple, List, Dict, Any
 
+import numpy as np
+from datasets import load_from_disk
 from torch.nn import CrossEntropyLoss
-from transformers import LlamaTokenizer, LlamaForCausalLM, LlamaPreTrainedModel, LlamaModel, AutoTokenizer, AutoConfig
-from transformers.modeling_outputs import SequenceClassifierOutput, CausalLMOutputWithPast, \
-    SequenceClassifierOutputWithPast
+from transformers import LlamaForCausalLM, AutoTokenizer, AutoModel
+from transformers.modeling_outputs import SequenceClassifierOutputWithPast
 from torch import nn
-from torch.nn.functional import softmax
 import torch
-import util
+
+from tokenization_alpaca import ALPACA_TASK_DESCRIPTION
+from util import get_args
 
 
-class LlamaForZeroShotSequenceClassification(LlamaForCausalLM):
-    def __init__(self, config):
-        super().__init__(config)
+class LlamaForZeroShotSequenceClassification(nn.Module):
+    def __init__(self, config, cache_dir, torch_compile=False):
+        super().__init__()
+        self.model = LlamaForCausalLM.from_pretrained(config, cache_dir=cache_dir)
+        self.hidden_size = self.model.config.hidden_size
+        self.vocab_size = self.model.config.vocab_size
+        if torch_compile:
+            self.model = torch.compile(self.model)
 
     def forward(
         self,
@@ -38,7 +47,7 @@ class LlamaForZeroShotSequenceClassification(LlamaForCausalLM):
         if input_ids is not None:
             input_ids = input_ids.view(-1, seq_len)
         if inputs_embeds is not None:
-            inputs_embeds = inputs_embeds.view(-1, seq_len, 4096)
+            inputs_embeds = inputs_embeds.view(-1, seq_len, self.hidden_size)
         if attention_mask is not None:
             attention_mask = attention_mask.view(-1, seq_len)
         if labels is not None:
@@ -46,7 +55,7 @@ class LlamaForZeroShotSequenceClassification(LlamaForCausalLM):
 
         assert "classification_labels" in kwargs
 
-        decoder_outputs = super().forward(
+        decoder_outputs = self.model(
             input_ids,
             attention_mask=attention_mask,
             position_ids=position_ids,
@@ -62,7 +71,7 @@ class LlamaForZeroShotSequenceClassification(LlamaForCausalLM):
         shift_logits = decoder_outputs.logits[..., :-1, :].contiguous()
         shift_labels = labels[..., 1:].contiguous()
         loss_fct = CrossEntropyLoss(reduction="none")
-        shift_logits = shift_logits.view(-1, self.config.vocab_size)
+        shift_logits = shift_logits.view(-1, self.vocab_size)
         shift_labels = shift_labels.view(-1)
         shift_labels = shift_labels.to(shift_logits.device)
         loss = loss_fct(shift_logits, shift_labels).view(batch_size * n_labels, -1)
@@ -88,33 +97,37 @@ class LlamaForZeroShotSequenceClassification(LlamaForCausalLM):
             attentions=decoder_outputs.attentions,
         )
 
+    def generate(self, **kwargs):
+        return self.model.generate(**kwargs)
+
 
 class ZeroShotLlamaForSemAttack(nn.Module):
-    def __init__(self, model_name, cache_dir):
+    def __init__(self, model_name, cache_dir, torch_compile=False):
         super().__init__()
-        self.llama_classifier = LlamaForZeroShotSequenceClassification.from_pretrained(model_name, cache_dir=cache_dir)
+        self.torch_compile = torch_compile
+        self.llama_classifier = LlamaForZeroShotSequenceClassification(
+            model_name, cache_dir, torch_compile=torch_compile
+        )
 
     def get_input_embedding_vector(self, input_ids: torch.LongTensor):
-        return self.llama_classifier.get_input_embeddings()(input_ids)
+        return self.llama_classifier.model.get_input_embeddings()(input_ids)
 
     def forward(
         self, input_dict: Dict[str, Union[torch.Tensor, List]], gold=None, perturbed=None, **kwargs
     ) -> Dict[str, Any]:
         # Assuming single input_embedding for now...
-        instruction_token_embedding = self.llama_classifier.get_input_embeddings()(input_dict["instruction_token_ids"])
+        instruction_token_embedding = self.get_input_embedding_vector(input_dict["instruction_token_ids"])
         if perturbed is not None:
             input_token_embedding = perturbed
         else:
-            input_token_embedding = self.llama_classifier.get_input_embeddings()(input_dict["input_token_ids"])
-        response_header_token_embedding = self.llama_classifier.get_input_embeddings()(
-            input_dict["response_header_token_ids"]
-        )
+            input_token_embedding = self.get_input_embedding_vector(input_dict["input_token_ids"])
+        response_header_token_embedding = self.get_input_embedding_vector(input_dict["response_header_token_ids"])
         inputs_embeds = torch.stack([
             torch.cat([
                 instruction_token_embedding,
                 input_token_embedding,
                 response_header_token_embedding,
-                self.llama_classifier.get_input_embeddings()(input_dict[f"{label}_response_token_ids"])
+                self.get_input_embedding_vector(input_dict[f"{label}_response_token_ids"])
             ]) for label in input_dict["label_names"]
         ])  # (num_labels, seq_len, 4096)
 
@@ -154,28 +167,46 @@ class ZeroShotLlamaForSemAttack(nn.Module):
 
 def test():
     from tqdm import tqdm
-    from datasets import load_dataset
 
+    args = get_args()
     device = torch.device("cuda:0")
 
-    test_data = load_dataset("glue", "sst2", cache_dir="./.cache/", split="validation")
-    test_data = test_data.load_from_disk(f"./.cache/sst2/")
-    test_data.set_format("pt", output_all_columns=True)
+    tokenizer = AutoTokenizer.from_pretrained(args.model, cache_dir=args.cache_dir)
+    model = ZeroShotLlamaForSemAttack(args.model, cache_dir=args.cache_dir, torch_compile=False)
+    model.to(device=device, dtype=torch.bfloat16).eval()
 
-    model = ZeroShotLlamaForSemAttack("chavinlo/alpaca-native", cache_dir="./.cache/")
-    model.to(device=device, dtype=torch.bfloat16)
-    model.eval()
+    for task in ALPACA_TASK_DESCRIPTION.keys():
+        data_dir = os.path.join(args.cache_dir, f"glue-preprocessed-benign", args.model, task)
+        test_data = load_from_disk(data_dir)
+        test_data.set_format("pt", output_all_columns=True)
 
-    sst2_dev_predictions = []
-    for data in tqdm(test_data):
-        data = {k: v.to(device) if isinstance(v, torch.Tensor) else v for k, v in data.items()}
-        pred = model(data, perturbed=torch.randn(50, 4096, device=device))
-        print(pred["pred"].reshape(-1).argmax().item())
-        sst2_dev_predictions.append(pred["pred"].reshape(-1).argmax().item())
+        generation_predictions = []
+        dev_predictions = []
+        dev_labels = []
+        for data in tqdm(test_data):
+            data = {k: v.to(device) if isinstance(v, torch.Tensor) else v for k, v in data.items()}
+            pred = model(data)
+            generate_ids = model.llama_classifier.generate(
+                input_ids=data["input_ids"][:data["label_start_idx"]].unsqueeze(dim=0), max_new_tokens=5
+            )
+            generated_prediction = tokenizer.batch_decode(generate_ids)
+            print(generated_prediction, tokenizer.batch_decode(data["input_ids"][:data["label_start_idx"]]))
+            generation_predictions.append(generated_prediction)
+            dev_predictions.append(pred["pred"].reshape(-1).argmax().item())
+            dev_labels.append(data["label"].item())
+        print(task, np.mean(np.array(dev_labels) == np.array(dev_predictions)))
 
-    with open("./adv-glue/sst-2-dev-alpaca-zeroshot.json", "w") as f:
-        import json
-        json.dump(sst2_dev_predictions, f, indent=4)
+        dest_path = os.path.join(f"./adv-glue/", args.model, task, "benign-zeroshot.json")
+        if not os.path.exists(os.path.dirname(dest_path)):
+            os.makedirs(os.path.dirname(dest_path))
+        with open(dest_path, "w") as f:
+            json.dump(
+                {
+                    "labels": dev_labels,
+                    "lm_predictions": dev_predictions,
+                    "generation_predictions": generation_predictions
+                }, f, indent=4
+            )
 
 
 if __name__ == "__main__":
